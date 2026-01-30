2f866db6b270830ffd6e30a6a2d73d59c7f2e915
J1G54W-999 <shae_haseldine@protonmail.com>
2026-01-30 16:49:59 +1030

    portfolio update

---
+++ assets/javascript/scripts.js
@@ -0,0 +1,32 @@
+
+// javascript for tabs //
+
+function openTab(evt, tabName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Opens the profile page by default
+document.addEventListener('DOMContentLoaded', function() {
+  // Simulate clicking the profile tab button
+  var profileButton = document.querySelector('.tablinks[onclick="openTab(event, \'profile\')"]');
+  if (profileButton) {
+    profileButton.click();
+  }
+});
